db8984e193c89d5dddf0fda8803f78e9fdfefb31
Kyle Geske <stungeye@gmail.com>
2020-10-28 13:47:11 -0500

    Ran all pending migrations.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+NUMBER_OF_BREEDS = 2
+HORSES_PER_BREED = 4
+
+NUMBER_OF_BREEDS.times do
+  breed = Breed.create(name: Faker::Creature::Horse.unique.breed)
+
+  HORSES_PER_BREED.times do
+    horse = breed.horses.create(
+      name:      Faker::Creature::Horse.unique.name,
+      age:       rand(3..348),
+      top_speed: 3.1415926 * rand(20..56)
+    )
+  end
+end
+
+puts "Created #{Breed.count} Breeds."
+puts "Created #{Horse.count} Horses."
