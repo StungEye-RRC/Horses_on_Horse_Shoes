a5145c289210fe655ef3705635935927083ebeec
Kyle Geske <stungeye@gmail.com>
2020-11-02 12:34:46 -0600

    Added support for horse number of legs to seed script and horse partial.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,10 @@ NUMBER_OF_BREEDS.times do
 
   HORSES_PER_BREED.times do
     horse = breed.horses.create(
-      name:      Faker::Creature::Horse.unique.name,
-      age:       rand(3..348),
-      top_speed: 3.1415926 * rand(20..56)
+      name:           Faker::Creature::Horse.unique.name,
+      age:            rand(3..348),
+      top_speed:      3.1415926 * rand(20..56),
+      number_of_legs: rand(1..14)
     )
   end
 end
